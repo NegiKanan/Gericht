2461bda4991b4abe069158d17d1a0c07e09d2ce8
NegiKanan <138270622+NegiKanan@users.noreply.github.com>
2024-11-19 18:52:33 +0530

    Add files via upload

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Header.css';
+
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key To Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>Gericht is a contemporary restaurant known for its fusion of bold flavors and artistic presentation. Renowned for its dedication to locally-sourced ingredients, the menu showcases a creative twist on classic dishes. </p>
+      <a href="#menu">
+      <button type="button" className="custom__button">Explore Menu</button>
+      </a>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header_img" />
+    </div>
+  </div>
+);
+
+export default Header;
