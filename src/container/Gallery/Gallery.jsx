aa059c4e07048d1dfda4b7fa52480821c61d64b0
NegiKanan <138270622+NegiKanan@users.noreply.github.com>
2024-11-19 18:51:50 +0530

    Add files via upload

---
+++ src/container/Gallery/Gallery.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Gallery.css';
+
+const Gallery = () => {
+  const scrollRef = React.useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Our photo gallery highlights the creativity and passion that define every dish and dining experience at Gericht.</p>
+        <a href="https://in.pinterest.com/negikanan/">
+        <button type="button" className="custom__button">View More</button>
+        </a>
+      </div>
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery_image" />
+              <a href="https://www.instagram.com/negikanan_">
+          <BsInstagram className="gallery__image-icon" />
+        </a>                 
+        </div>
+          ))}
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
